056954525b8b68591341f7298a109066c6c43973
abdulhannanqureshi <qhannan60@gmail.com>
2025-08-02 11:22:43 +0530

    fix expiry date in token model

--- nodejs-base-typeScript-mongodb-graphql/src/models/token.model.ts
+++ nodejs-base-typeScript-mongodb-graphql/src/models/token.model.ts
@@ -4,16 +4,8 @@ import { TokenType } from '@/enums';
 const TokenSchema: Schema = new Schema({
   token: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  type: {
-    type: String,
-    enum: TokenType,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    // expires: process.env.JWT_REFRESH_EXPIRATION,
-  },
-});
+  type: { type: String, enum: TokenType, required: true },
+
+}, { timestamps: true });
 
 export default mongoose.model('Token', TokenSchema);
--- nodejs-base-typeScript-mongodb/src/models/token.model.ts
+++ nodejs-base-typeScript-mongodb/src/models/token.model.ts
@@ -5,16 +5,8 @@ const TokenSchema: Schema = new Schema({
   token: { type: String, required: true },
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   used: { type: Boolean, default: false },
-  type: {
-    type: String,
-    enum: TokenType,
-    required: true,
-  },
-  created_at: {
-    type: Date,
-    default: Date.now,
-    // expires: process.env.JWT_REFRESH_EXPIRATION,
-  },
-});
+  type: { type: String, enum: TokenType, required: true },
+
+}, { timestamps: true });
 
 export default mongoose.model('Token', TokenSchema);
